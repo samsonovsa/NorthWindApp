9235bd85ae07a04d23932206a9ec770d2efeb4fa
Sergey Samsonov <samsonovsa@yandex.ru>
2020-07-18 09:00:45 +0300

    Add CRUD viewa for product

--- NorthWindApp/Controllers/ProductController.cs
+++ NorthWindApp/Controllers/ProductController.cs
@@ -3,27 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using NorthWindApp.Models.DataModels;
 using NorthWindApp.Models.Entities;
+using NorthWindApp.Models.ViewModels;
 
 namespace NorthWindApp.Controllers
 {
     public class ProductController : Controller
     {
         IUnitOfWork _unitOfWork;
+        IConfiguration _configuration;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IConfiguration configuration)
         {
             _unitOfWork = unitOfWork;
+            _configuration = configuration;
         }
 
         public async Task<ActionResult> Index()
         {
             var products = await Task<IEnumerable<Product>>
-                .Run(()=> _unitOfWork.Products
-                    .GetWithInclude(p=>p.Category,p=> p.Supplier));
+                .Run(() => _unitOfWork.Products
+                    .GetWithInclude(p => p.Category, p => p.Supplier));
 
-            return View(products);
+            var productsViewModel = new ProductsViewModel(_configuration);
+            productsViewModel.SetProducts(products);
+
+            return View(productsViewModel.Products);
+        }
+
+        [HttpGet()]
+        public ActionResult Create()
+        {
+            return  View();
+        }
+
+        [HttpPost()]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                    await Task.Run(() => _unitOfWork.Products.Create(product));
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult> Update(int id)
+        {
+            Product product = await Task.Run(()=> _unitOfWork.Products.FindById(id));
+            return View(product);
+        }
+
+        [HttpPost()]
+        [ValidateAntiForgeryToken]
+        public async Task Update(Product product)
+        {
+            if (ModelState.IsValid)
+                await Task.Run(() => _unitOfWork.Products.Update(product));
+        }
+
+        [HttpPost()]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(Product product)
+        {
+            try
+            {
+                await Task.Run(() => _unitOfWork.Products.Remove(product));
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         protected override void Dispose(bool disposing)
---
+++ NorthWindApp/Models/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using NorthWindApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthWindApp.Models.ViewModels
+{
+    public class ProductsViewModel
+    {
+        private int _maxProductCountOnPage;
+
+        public ProductsViewModel(IConfiguration configuration)
+        {
+            int.TryParse(configuration["MaxProductCountOnPage"],out _maxProductCountOnPage);
+            Products = new List<Product>();
+        }
+
+        public int CountProductOnPage => _maxProductCountOnPage;
+
+        public List<Product> Products { get; set; }
+
+        public void SetProducts(IEnumerable<Product> products)
+        {
+            Products = CountProductOnPage > 0 ? 
+                products.Take(CountProductOnPage).ToList()
+                : products.ToList();            
+        }
+
+    }
+}
