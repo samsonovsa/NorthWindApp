631115be9851eb8f12a4d1733add41ce21022301
Sergey Samsonov <samsonovsa@yandex.ru>
2020-07-26 07:50:42 +0300

    CategoryController Test

---
+++ NorthWindApp.Test/CategoryControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using Moq;
+using NorthWindApp.BLL.Interfaces;
+using NorthWindApp.Controllers;
+using System.Collections.Generic;
+using NorthWindApp.DTO.Models;
+using Microsoft.AspNetCore.Mvc;
+using NorthWindApp.Models.ViewModels;
+
+namespace NorthWindApp.Test
+{
+    public class CategoryControllerTest
+    {
+        [Fact]
+        public void Index_ReturnsAViewResult_WithA()
+        {
+            // Arrange
+            var mockDictonaryService = new Mock<IDictionaryService>();
+            mockDictonaryService.Setup(service => service.GetCategoriesAsync())
+                .ReturnsAsync(GetCategories());
+            var controller = new CategoryController(mockDictonaryService.Object);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<CategoryViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+        private IEnumerable<Category> GetCategories()
+        {
+            return new List<Category>()
+            {
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Category 1"
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Category 2"
+                }
+            };
+        }
+    }
+}
--- NorthWindApp/Controllers/CategoryController.cs
+++ NorthWindApp/Controllers/CategoryController.cs
@@ -1,21 +1,27 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NorthWindApp.BLL.Interfaces;
+using AutoMapper;
+using NorthWindApp.Models.ViewModels;
+using System.Collections.Generic;
 
 namespace NorthWindApp.Controllers
 {
     public class CategoryController : Controller
     {
         IDictionaryService _dictionaryService;
+        IMapper _mapper;
 
-        public CategoryController(IDictionaryService dictionaryService)
+        public CategoryController(IDictionaryService dictionaryService, IMapper mapper)
         {
             _dictionaryService = dictionaryService;
+            _mapper = mapper;
         }
 
         public async Task<ActionResult> Index()
         {
-            var categories = await _dictionaryService.GetCategoriesAsync();
+            var categories =  _mapper.Map<IEnumerable<CategoryViewModel>>(
+                await _dictionaryService.GetCategoriesAsync());
             return View(categories);
         }
 
